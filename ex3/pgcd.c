6b5df5e0d8596911b0fb02d48f7c83c2e9f5a825
Kghesquier <kiliangppg@gmail.com>
2026-05-05 20:33:31 +0200

    Updated

--- ex3/pgcd.c
+++ ex3/pgcd.c
@@ -6,7 +6,7 @@ int ft_atoi(const char *str)
     int res = 0;
     int sign = 1;
 
-    while (str[i] == ' ' || str[i] >= 9 && str[i] <= 13)
+    while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
         i++;
     if (str[i] == '-' || str[i] == '+')
     {
@@ -26,11 +26,27 @@ int main(int argc, char **argv)
 {
     int a;
     int b;
+    int i;
 
     if (argc != 3)
     {
         printf("\n");
         return (0);
     }
-    
+    a = ft_atoi(argv[1]);
+    b = ft_atoi(argv[2]);
+    if (a > b)
+        i = b;
+    else
+        i = a;
+    while (i > 0)
+    {
+        if (a % i == 0 && b % i == 0)
+        {
+            printf("%d\n", i);
+            return (0);
+        }
+        i--;
+    }
+    return (0);
 }
